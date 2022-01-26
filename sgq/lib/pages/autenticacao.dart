8b57d622be861e604ef37980f3b0f8363a66b8fb
bruna kailane <kailanefcordeiro@gmail.com>
2022-01-26 16:54:21 -0300

    layout login

--- sgq/lib/pages/autenticacao.dart
+++ sgq/lib/pages/autenticacao.dart
@@ -50,81 +50,131 @@ class _AutenticacaoState extends State<Autenticacao> {
         Provider.of<AutenticacaoServico>(context, listen: false);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SGQ'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              _Formulario(
-                label: "E-mail",
-                hint: "Digite seu e-mail",
-                icon: const Icon(Icons.email),
-                validator: (text) {
-                  if (text == null || text.isEmpty) {
-                    return "Email obrigatorio";
-                  }
-                  if (!text.contains("@") || !text.contains(".")) {
-                    return "Digite um e-mail valido";
-                  }
-                },
-                save: (text) {
-                  setState(() {
-                    email = text ?? '';
-                  });
-                },
-              ),
-              const SizedBox(height: 8),
-              _Formulario(
-                label: "Senha",
-                hint: "Digite sua senha",
-                obscureText: ocultarSenha,
-                icon: const Icon(Icons.vpn_key),
-                sufixIcon: IconButton(
-                  icon: Icon(
-                      ocultarSenha ? Icons.visibility : Icons.visibility_off),
-                  onPressed: mudarOcultarSenha,
+      backgroundColor: Colors.white,
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Stack(
+          children: <Widget>[
+            Image.asset('assets/imagem/sport.jpg', fit: BoxFit.cover),
+            const Positioned(
+              top: 140,
+              left: 32,
+              child: Text(
+                "Sign In",
+                style: TextStyle(
+                  fontSize: 42,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
-                validator: (text) {
-                  if (text == null || text.isEmpty) {
-                    return "Senha obrigatoria";
-                  }
-                  if (text.length < 6) {
-                    return "A senha precisa ser maior que 6 caracteres";
-                  }
-                },
-                save: (text) {
-                  setState(() {
-                    senha = text ?? '';
-                  });
-                },
               ),
-              const SizedBox(height: 20),
-              processando
-                  ? const CircularProgressIndicator()
-                  : Column(
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            submeter(autenticacaoServ);
-                          },
-                          child: const Text("Login"),
+            ),
+            Positioned(
+              top: 200,
+              child: Container(
+                padding: const EdgeInsets.all(50),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(52),
+                    topRight: Radius.circular(52),
+                  ),
+                ),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      const Icon(
+                        Icons.people_alt_rounded,
+                        color: Colors.green,
+                        size: 80,
+                      ),
+                      _Formulario(
+                        label: "E-mail",
+                        hint: "Digite seu e-mail",
+                        icon: const Icon(Icons.email),
+                        validator: (text) {
+                          if (text == null || text.isEmpty) {
+                            return "Email obrigatorio";
+                          }
+                          if (!text.contains("@") || !text.contains(".")) {
+                            return "Digite um e-mail valido";
+                          }
+                        },
+                        save: (text) {
+                          setState(() {
+                            email = text ?? '';
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 8),
+                      _Formulario(
+                        label: "Senha",
+                        hint: "Digite sua senha",
+                        obscureText: ocultarSenha,
+                        icon: const Icon(Icons.vpn_key),
+                        sufixIcon: IconButton(
+                          icon: Icon(ocultarSenha
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                          onPressed: mudarOcultarSenha,
                         ),
-                      ],
-                    ),
-              if (erro.isNotEmpty) const SizedBox(height: 50),
-              if (erro.isNotEmpty)
-                Text(
-                  erro,
-                  style: const TextStyle(color: Colors.red),
+                        validator: (text) {
+                          if (text == null || text.isEmpty) {
+                            return "Senha obrigatoria";
+                          }
+                          if (text.length < 6) {
+                            return "A senha precisa ser maior que 6 caracteres";
+                          }
+                        },
+                        save: (text) {
+                          setState(() {
+                            senha = text ?? '';
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      processando
+                          ? const CircularProgressIndicator()
+                          : Column(
+                              children: [
+                                SizedBox(
+                                  width: 250,
+                                  height: 50,
+                                  child: ElevatedButton(
+                                    style: ButtonStyle(
+                                        shape: MaterialStateProperty.all<
+                                                RoundedRectangleBorder>(
+                                            RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        30)))),
+                                    onPressed: () {
+                                      submeter(autenticacaoServ);
+                                    },
+                                    child: const Text(
+                                      "Entrar",
+                                      style: TextStyle(
+                                          fontSize: 32, color: Colors.white),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                      if (erro.isNotEmpty) const SizedBox(height: 50),
+                      if (erro.isNotEmpty)
+                        Text(
+                          erro,
+                          style: const TextStyle(color: Colors.red),
+                        ),
+                    ],
+                  ),
                 ),
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -167,7 +217,7 @@ class _Formulario extends StatelessWidget {
         labelText: label,
         hintText: hint,
         hintStyle: const TextStyle(color: Colors.grey),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(40)),
+        //  border: OutlineInputBorder(borderRadius: BorderRadius.circular(40)),
       ),
     );
   }
